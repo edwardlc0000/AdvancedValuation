f14c80f158d7a2bf0f4caafe8f29714eb0942f66
Edward Cromwell <edwardlc@umich.edu>
2025-04-04 20:11:23 -0400

    Add project files.

---
+++ Enterprise.py
@@ -0,0 +1,18 @@
+# A class modeling the financial statements of an enterprise
+
+import pandas as pd
+
+class Enterprise:
+
+    def __init__(self, name, revenue, expenses, assets, liabilities):
+        self.name = name
+        self.revenue = revenue
+        self.expenses = expenses
+        self.assets = assets
+        self.liabilities = liabilities
+
+    pass
+
+
+
+

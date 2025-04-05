2391d3835f0e0910548aab28d117e1a8cff55f78
Edward Cromwell <edwardlc@umich.edu>
2025-04-04 22:10:06 -0400

    feat (ImportWizard): configured ImportWizard and CapIQ template

--- ImportWizard.py
+++ ImportWizard.py
@@ -2,9 +2,15 @@ import pandas as pd
 import wx
 from pathlib import Path
 
+na_values = ['', '#N/A', '#N/A N/A', '#NA',
+             '-1.#IND', '-1.#QNAN', '-NaN',
+             '-nan', '1.#IND', '1.#QNAN',
+             '<NA>', 'N/A', 'NA', 'NULL',
+             'NaN', 'None', 'n/a', 'nan',
+             'null']
+
 def import_statements() -> pd.DataFrame:
-    
-    
+
     app = wx.App(False)
 
     dialog = wx.FileDialog(
@@ -17,21 +23,26 @@ def import_statements() -> pd.DataFrame:
     if dialog.ShowModal() == wx.ID_OK:
         file_name = dialog.GetPath()
     else:
-        print("No file selected.")
         dialog.Destroy()
         app.Destroy()
-        return {}
+        raise FileNotFoundError("No file selected.")
 
     dialog.Destroy()
     app.Destroy()
 
     file_path: Path = Path(file_name)
-    
-    df = pd.read_xlsx(file_path)
+
+    # noinspection PyTypeChecker
+    df: pd.DataFrame = pd.read_excel(file_path,
+                                     sheet_name="Financial Statements",
+                                     index_col=0,
+                                     header=9,
+                                     usecols=range(0, 11),
+                                     na_values=na_values)
 
     return df
 
 
 if __name__ == "__main__":
-    df = import_statements()
-    print(df.head())
+    test_df = import_statements()
+    print(test_df.head(10))

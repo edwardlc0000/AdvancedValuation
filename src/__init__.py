c7c94fc5d6dc4c822990aa0293c2a47d1c5c9e49
Edward Cromwell <edwardlc@umich.edu>
2025-04-06 09:33:26 -0400

    chore: added __init__.py files to directories

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+"""
+Advanced Valuation Package for financial analysis and modeling.
+"""
+
+# Import main classes to make them available at package level
+from .Enterprise import Enterprise
+from .ProjectionEngine import ProjectionEngine
+
+# Version info
+__version__ = '0.1.0'

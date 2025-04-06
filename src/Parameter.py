b971b03ee92d23fa53e51ed644076149d224bc36
Edward Cromwell <edwardlc@umich.edu>
2025-04-06 19:38:35 -0400

    feat (Parameter): implemented ability to set standard deviation of Parameter instance for simulation

--- src/Parameter.py
+++ src/Parameter.py
@@ -2,8 +2,12 @@
 import numpy as np
 
 class Parameter:
-    def __init__(self, data: np.ndarray, distribution: str = 'Normal'):
+
+    def __init__(self, data: np.ndarray, std: np.floating = None, distribution: str = 'Normal'):
         self.data: np.ndarray = data
         self.mean: np.floating = np.mean(data)
-        self.std: np.floating = np.std(data)
+        if std is None:
+            self.std: np.floating = np.std(data)
+        else:
+            self.std: np.floating = std
         self.distribution: str = distribution

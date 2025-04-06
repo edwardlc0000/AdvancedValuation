0543a699120f18675af97615ef2aa0301f2ac133
Edward Cromwell <edwardlc@umich.edu>
2025-04-06 18:11:19 -0400

    wip (ProjectionEngine): added new utility functions to correlation matrix calculation

--- src/ProjectionEngine.py
+++ src/ProjectionEngine.py
@@ -5,7 +5,10 @@ import pandas as pd
 from .Enterprise import Enterprise
 from .ProjectionUtils import (
     calc_revenue_growth,
+    calc_cogs_revenue,
     calc_r_and_d_revenue,
+    calc_sga_revenue,
+    calc_da_prior_nppe,
     calc_ucoe,
     calc_reinvestment_rate,
     calc_growth
@@ -23,8 +26,17 @@ class ProjectionEngine:
             raise ValueError("Cannot calculate correlation with empty data")
 
         revenue_growth: np.ndarray = calc_revenue_growth(self.enterprise.income_statement.loc['Revenue'])
+        cogs_revenue: np.ndarray = calc_cogs_revenue(self.enterprise.income_statement.loc['Revenue'],
+                                                     self.enterprise.income_statement.loc['Cost of Goods Sold'])
         r_and_d_revenue: np.ndarray = calc_r_and_d_revenue(self.enterprise.income_statement.loc['Revenue'],
                                                            self.enterprise.income_statement.loc['R&D Exp.'])
+        sga_revenue: np.ndarray = calc_sga_revenue(self.enterprise.income_statement.loc['Revenue'],
+                                                   self.enterprise.income_statement.loc['Selling General & Admin Exp.'])
+        da_nppe: np.ndarray = calc_da_prior_nppe(self.enterprise.cash_flow_statement.loc['Depreciation & Amort.'],
+                                                 self.enterprise.balance_sheet.loc['Net Property Plant & Equipment'])
         data_matrix: np.ndarray = np.vstack([revenue_growth,
-                                             r_and_d_revenue])
+                                             cogs_revenue,
+                                             sga_revenue,
+                                             r_and_d_revenue,
+                                             da_nppe])
         self.correlation_matrix = np.corrcoef(data_matrix)
